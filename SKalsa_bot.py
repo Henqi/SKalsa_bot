9b83a713f42f798b86af7ac5ef62f92e5d2960ab
Henri Heilala <contact@henriheilala.fi>
2023-03-21 23:34:10 +0200

    add user_id into request to get data for specific court & add additional command

--- SKalsa_bot.py
+++ SKalsa_bot.py
@@ -32,9 +32,9 @@ async def hakis(update: Update, context: ContextTypes.DEFAULT_TYPE):
     branch_id = '2b325906-5b7a-11e9-8370-fa163e3c66dd'
     group_id = 'a17ccc08-838a-11e9-8fd9-fa163e3c66dd'
     product_id = '59305e30-8b49-11e9-800b-fa163e3c66dd'
+    user_id = 'd7c92d04-807b-11e9-b480-fa163e3c66dd' #kenttä2
 
-
-    day_url = 'https://avoinna24.fi/api/slot?filter[ismultibooking]=' + ismultibooking + '&filter[branch_id]=' + branch_id + '&filter[group_id]=' + group_id + '&filter[product_id]=' + product_id + '&filter[date]=' + day_as_string + '&filter[start]=' + day_as_string + '&filter[end]=' + day_as_string
+    day_url = 'https://avoinna24.fi/api/slot?filter[ismultibooking]=' + ismultibooking + '&filter[branch_id]=' + branch_id + '&filter[group_id]=' + group_id + '&filter[product_id]=' + product_id + '&filter[user_id]=' + user_id + '&filter[date]=' + day_as_string + '&filter[start]=' + day_as_string + '&filter[end]=' + day_as_string
     headers = {'X-Subdomain': 'arenacenter'}
     
     reservation_data = requests.get(day_url, headers=headers)
@@ -52,6 +52,34 @@ async def hakis(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
+async def deloitte(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    today = date.today()
+    day = get_next_same_weekday(today, 2)
+    day_as_string = day.strftime('%Y-%m-%d')
+    hour='19'
+    ismultibooking = 'false'
+    branch_id = '2b325906-5b7a-11e9-8370-fa163e3c66dd'
+    group_id = 'a17ccc08-838a-11e9-8fd9-fa163e3c66dd'
+    product_id = '59305e30-8b49-11e9-800b-fa163e3c66dd'
+    user_id = 'ea8b1cf4-807b-11e9-93b7-fa163e3c66dd' #kenttä3
+
+    day_url = 'https://avoinna24.fi/api/slot?filter[ismultibooking]=' + ismultibooking + '&filter[branch_id]=' + branch_id + '&filter[group_id]=' + group_id + '&filter[product_id]=' + product_id + '&filter[user_id]=' + user_id + '&filter[date]=' + day_as_string + '&filter[start]=' + day_as_string + '&filter[end]=' + day_as_string
+    headers = {'X-Subdomain': 'arenacenter'}
+    
+    reservation_data = requests.get(day_url, headers=headers)
+    reservation_json = reservation_data.json()
+
+    if len(reservation_json['data']) > 0:
+        for slot in reservation_json['data']:
+            if day_as_string + ' ' + hour in slot['attributes']['endtime']:
+                result = "Päivälle " + day_as_string + " on vapaana vuoro joka loppuu tunnilla " + hour 
+                break
+        result = "Päivälle " + day_as_string + " EI OLE vapaata vuoroa joka loppuu tunnilla " + hour 
+    
+    else:
+        result = "Päivälle " + day_as_string + " ei löytynyt yhtään vapaata vuoroa / dataa ei löytynyt"
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(api_key).build()
@@ -61,5 +89,8 @@ if __name__ == '__main__':
 
     hakis_handler = CommandHandler('hakis', hakis)
     application.add_handler(hakis_handler)
+
+    deloitte_handler = CommandHandler('deloitte', deloitte)
+    application.add_handler(deloitte_handler)
     
     application.run_polling()
