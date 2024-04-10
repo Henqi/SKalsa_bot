8c5146be81c2e5b62acb182fb8bee249b037c4dd
Henri Heilala <henri.heilala@nitor.com>
2024-04-10 22:18:44 +0300

    test: fix tests

--- tests/skalsa_bot_test.py
+++ tests/skalsa_bot_test.py
@@ -8,11 +8,14 @@ from src.skalsa_bot import get_next_same_weekday
 @pytest.mark.parametrize(
     "date, day, expected_result",
     [
-        (datetime.datetime(2024, 4, 1), 5, datetime.datetime(2024, 4, 5)),
-        (datetime.datetime(2024, 4, 1), 1, datetime.datetime(2024, 4, 2)),
-        (datetime.datetime(2024, 4, 1), 3, datetime.datetime(2024, 4, 4)),
-        (datetime.datetime(2024, 4, 1), 6, datetime.datetime(2024, 4, 6)),
-        (datetime.datetime(2024, 4, 1), 7, datetime.datetime(2024, 4, 7)),
+        (datetime.datetime(2024, 4, 1), 1, datetime.datetime(2024, 4, 1)),
+        (datetime.datetime(2024, 4, 2), 2, datetime.datetime(2024, 4, 2)),
+        (datetime.datetime(2024, 4, 2), 3, datetime.datetime(2024, 4, 3)),
+        (datetime.datetime(2024, 4, 4), 3, datetime.datetime(2024, 4, 10)),
+        (datetime.datetime(2024, 4, 6), 5, datetime.datetime(2024, 4, 12)),
+        (datetime.datetime(2024, 4, 6), 6, datetime.datetime(2024, 4, 6)),
+        (datetime.datetime(2024, 4, 7), 1, datetime.datetime(2024, 4, 8)),
+        (datetime.datetime(2024, 4, 8), 7, datetime.datetime(2024, 4, 14)),
     ],
 )
 def test_get_next_same_weekday_valid_input(date, day, expected_result):
