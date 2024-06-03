fb9d860dfff331955272d3e61a1a9a652105e7c3
Henri Heilala <contact@henriheilala.fi>
2024-06-03 23:34:26 +0300

    feat: remove rust implementation

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use reqwest;
-
-// tokio let's us use "async" on our main function
-#[tokio::main]
-async fn main() {
-    // chaining .await will yield our query result
-    let result = reqwest::get("https://api.spotify.com/v1/search").await;
-    println!("{:?}", result);
-}
